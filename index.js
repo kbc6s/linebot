27e6112b1c739d4acbcecb0f12c4b45a565978e4
kaiwen <f129546497@gmail.com>
2018-06-30 03:30:43 +0800

    xxxyyzzz

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const line = require('@line/bot-sdk');
 const express = require('express');
+var app = express();
+// const lineConfig = {
+//   channelAccessToken: process.env.HEROKU_LINE_CHANNEL_ACCESS_TOKEN,
+//   channelSecret: process.env.HEROKU_LINE_CHANNEL_SECRET
+// };
+app.get("/linebot/eat", function(req, res){
+    res.send("吃吃吃")
+})
 const lineConfig = {
-  channelAccessToken: process.env.HEROKU_LINE_CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.HEROKU_LINE_CHANNEL_SECRET
-};
+    channelAccessToken: "UbOGgg5MnHJFlTVY1QVYDlfO9TsJqcKa9604kdK2VLNx4Gvlt8uELLGOiYHs+ELxIUPPNiNU9Hgc9KF+E609IOAmyZtl4MvjTSRJdwL50tbcaB9wsjTTfSDeEJAASPIo/1hQiuC0oJX0kDSQGvinpQdB04t89/1O/w1cDnyilFU=",
+    channelSecret: "00d47c75f006ede79fe0d6a665153103"
+  };
 function handleEvent(event) {
     switch (event.type) {
       case 'join':
@@ -22,15 +30,18 @@ function handleEvent(event) {
         }
     }
   }
-app.post('/', line.middleware(lineConfig), function(req, res) {
-    Promise
-      .all(req.body.events.map(handleEvent))
-      .then(function(result) {
-        res.json(result);
-      });
-  });
+  app.post("/post", function(req, res){
+    res.send("homie")
+})
+// app.post('/', line.middleware(lineConfig), function(req, res) {
+//     Promise
+//       .all(req.body.events.map(handleEvent))
+//       .then(function(result) {
+//         res.json(result);
+//       });
+//   });
 const client = new line.Client(lineConfig);
-const app = express();
+//const app = express();
 app.listen(3000, function() {
     console.log('App now running on port', this.address().port);
   });
