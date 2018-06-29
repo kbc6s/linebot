a2a743f53ae988eccddcac612eb767add3e569c1
kaiwen <f129546497@gmail.com>
2018-06-30 03:46:34 +0800

    rr2

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require('express');
 var app = express();
 
 const lineConfig = {
-  channelAccessToken: process.env.HEROKU_LINE_CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.HEROKU_LINE_CHANNEL_SECRET
+  channelAccessToken: process.env.channelAccessToken,
+  channelSecret: process.env.hannelSecret
 };
 app.get("/linebot/eat", function(req, res){
     res.send("吃吃吃")
@@ -31,13 +31,13 @@ function handleEvent(event) {
   app.post("/post", function(req, res){
     res.send("homie")
 })
-// app.post('/', line.middleware(lineConfig), function(req, res) {
-//     Promise
-//       .all(req.body.events.map(handleEvent))
-//       .then(function(result) {
-//         res.json(result);
-//       });
-//   });
+app.post('/', line.middleware(lineConfig), function(req, res) {
+    Promise
+      .all(req.body.events.map(handleEvent))
+      .then(function(result) {
+        res.json(result);
+      });
+  });
 const client = new line.Client(lineConfig);
 //const app = express();
 app.listen(3000, function() {
