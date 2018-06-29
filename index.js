d4a37185e978dd8a92532b9dfd8da02f29b0a042
kaiwen <f129546497@gmail.com>
2018-06-30 01:00:38 +0800

    First commit

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-var express = require("express");
-var app = express();
-
-var router = require("./routeConfig/route");
-app.use(router);
-app.use(express.static(__dirname + '/public'))
-app.set('view engine', 'html')
-var port = process.env.PORT || 3000;
-app.listen(port, function(){
-    console.log("Server up");
-})
+const line = require('@line/bot-sdk');
+const express = require('express');
+const lineConfig = {
+  channelAccessToken: process.env.HEROKU_LINE_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.HEROKU_LINE_CHANNEL_SECRET
+};
+function handleEvent(event) {
+    switch (event.type) {
+      case 'join':
+      case 'follow':
+        return client.replyMessage(event.replyToken, {
+          type: 'text',
+          text: '你好請問我們認識嗎?'
+        });   
+      case 'message':
+        switch (event.message.type) {
+          case 'text':
+            return client.replyMessage(event.replyToken, {
+              type: 'text',
+              text: (event.message.text+'~*')
+            });
+        }
+    }
+  }
+app.post('/', line.middleware(lineConfig), function(req, res) {
+    Promise
+      .all(req.body.events.map(handleEvent))
+      .then(function(result) {
+        res.json(result);
+      });
+  });
+const client = new line.Client(lineConfig);
+const app = express();
+app.listen(3000, function() {
+    console.log('App now running on port', this.address().port);
+  });
