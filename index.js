8d9887f3c598920d8f458b73d106e89861130649
kaiwen <f129546497@gmail.com>
2018-06-30 03:39:31 +0800

    rrr

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const line = require('@line/bot-sdk');
 const express = require('express');
 var app = express();
-// const lineConfig = {
-//   channelAccessToken: process.env.HEROKU_LINE_CHANNEL_ACCESS_TOKEN,
-//   channelSecret: process.env.HEROKU_LINE_CHANNEL_SECRET
-// };
+
+const lineConfig = {
+  channelAccessToken: process.env.HEROKU_LINE_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.HEROKU_LINE_CHANNEL_SECRET
+};
 app.get("/linebot/eat", function(req, res){
     res.send("吃吃吃")
 })
-const lineConfig = {
-    channelAccessToken: "UbOGgg5MnHJFlTVY1QVYDlfO9TsJqcKa9604kdK2VLNx4Gvlt8uELLGOiYHs+ELxIUPPNiNU9Hgc9KF+E609IOAmyZtl4MvjTSRJdwL50tbcaB9wsjTTfSDeEJAASPIo/1hQiuC0oJX0kDSQGvinpQdB04t89/1O/w1cDnyilFU=",
-    channelSecret: "00d47c75f006ede79fe0d6a665153103"
-  };
+
 function handleEvent(event) {
     switch (event.type) {
       case 'join':
